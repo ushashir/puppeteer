9bbe1bdf04ac44ce679be59032389a7e21b7ddb1
Ushahemba Shir <laleoict@gmail.com>
2020-10-12 11:03:09 +0100

    index.js errors

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const puppeteer = require('puppeteer');
 
-
-
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const dtitle = e.children[1].children[0].children[0].title
+// get dtitle, durl and dimgurl
+const dtitle = e.children[1].children[0].children[0].title
 const durl = e.children[1].children[1].children[0].children[1].href
 const dImgUrl = e.children[0].src
 
 console.log(`[${i}] Title: ${dtitle} URL: ${durl} Image URL: ${dImgUrl}`);
 
-  await page.goto('https://example.com');
-  await page.screenshot({path: 'example.png'});
-  
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
+  await page.goto('http://www.ciinigeria.com/resources/downloads?page=1');
+  await page.JSON.parse(JSON.stringify({dtitle, durl, dImgUrl})); 
+  
   await browser.close();
   
 })();
 
-// get title
 
